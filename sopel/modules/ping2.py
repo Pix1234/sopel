b15d7eaf0c867e8ef35bd32d56db711ffcea4b34
zppix1 <support@zppixballee.com>
2016-10-03 15:43:36 -0500

    Bug smash time: Trying to fix ping2

--- sopel/modules/ping2.py
+++ sopel/modules/ping2.py
@@ -4,7 +4,13 @@ ping.py - Sopel Ping Module
 Author: Sean B. Palmer, inamidst.com
 About: http://sopel.chat
 """
+""""
+Trying to fix ping2 it doesnt seem to work on irc. 
+"""
 from __future__ import unicode_literals, absolute_import, print_function, division
+
+import random
+from sopel.module import rule, priority, thread
 
 from sopel.module import rule, priority, thread, commands
 
